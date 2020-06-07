fe33568e72bfe3d95550f2fcfec737bb731ec914
JunwenLIU <Qswzl235@gmail.com>
2020-06-07 22:38:04 +0800

    Update hellow.py
    学到此处

--- hellow.py
+++ hellow.py
@@ -1,12 +1,32 @@
 """
 使用变量保存数据并进行加减乘除运算
 
-Version: 0.1
+Version: 0.01
 Author: 浅色文
 """
-a = 321
-b = 12
-print(a + b)    # 333
-print(a - b)    # 309
-print(a * b)    # 3852
-print(a / b)    # 26.75
+a = 235
+b = 5
+print(a + b)    # 240
+print(a - b)    # 230
+print(a * b)    # 1175
+print(a / b)    # 47.0
+
+'''
+使用type()检查变量地类型
+
+Version：0.01
+Author：浅色文
+'''
+
+a = 200
+b = 20.35
+c = 1 + 5j
+d = 'hello,world'
+e = True
+
+print(type(a)) #整数'int'
+print(type(b)) #浮点数'float'
+print(type(c)) #复数型'complex'
+print(type(d)) #字符串'str'
+print(type(e)) #布尔型 'bool'
+
